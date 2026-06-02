660c5de70bf4730b3f6777a164fd4eb913a1de6d
WulingArista <saptaandika53@gmail.com>
2026-06-02 14:00:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,8 @@
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Terinstal');
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Aktif');
-  e.waitUntil(clients.claim()); // Memaksa SW langsung mengambil alih
-});
-
-self.addEventListener('fetch', (e) => {
-  // Memberitahu Chrome bahwa kita merespons jaringan, walau hanya bypass
-  e.respondWith(fetch(e.request).catch(() => {
-    console.log('Tidak ada koneksi internet');
-  }));
+  caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => caches.delete(key)));
+  });
 });
